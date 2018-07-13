9939ca7c5274e02a5c7b1ba2bc9d1709309a90cf
Pa Yeng Thao <payengt24@gmail.com>
2018-07-12 21:39:48 -0500

    added enter name

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,16 @@
+import java.util.Scanner;
 public class helloWorld {
 
+
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello, World! My name is Pa Yeng Thao");
+        System.out.println("Enter name: ");
+ 
+
+        String token = sc.nextLine();
+
+        System.out.println("Hi, " + token);
     }
 
 }
